2393bc0b695245ce163a78847f896ec54e9c6968
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-10-12 12:29:30 +0530

    API added to get videos

--- server/routes/video.js
+++ server/routes/video.js
@@ -72,6 +72,18 @@ router.post("/thumbnail", (req, res) => {
 
 });
 
+router.get("/getVideos", (req, res) => {
+    Video.find().
+        populate('writer').
+        exec((err, videos) => {
+            if (err) {
+                return res.status(400).send(err);
+            } else {
+                res.status(200).json({ success: true, videos });
+            }
+        })
+})
+
 router.post("/uploadVideo", (req, res) => {
     const video = new Video(req.body);
 
